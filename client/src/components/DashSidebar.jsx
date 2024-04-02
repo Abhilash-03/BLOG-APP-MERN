8d29c776890a56d9a9839ffd691cec1e573e024a
Abhilash <abhilashjha335@gmail.com>
2024-04-03 02:02:49 +0530

    add comments and dashboard section in admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { Sidebar } from "flowbite-react"
 import { useEffect, useState } from "react";
-import { HiArrowSmRight, HiDocumentText, HiUser, HiUserGroup } from 'react-icons/hi'
+import { HiAnnotation, HiArrowSmRight, HiChartPie, HiDocumentText, HiUser, HiUserGroup } from 'react-icons/hi'
 import { Link, useLocation } from "react-router-dom";
 import { logoutSuccess } from "../redux/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
@@ -36,6 +36,17 @@ const DashSidebar = () => {
     <Sidebar className="w-full md:w-56">
     <Sidebar.Items>
       <Sidebar.ItemGroup className='flex flex-col gap-1'>
+      {currentUser && currentUser.isAdmin && (
+            <Link to='/dashboard?tab=dash'>
+              <Sidebar.Item
+                active={tab === 'dash' || !tab}
+                icon={HiChartPie}
+                as='div'
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
         <Link to='/dashboard?tab=profile'>
          <Sidebar.Item active ={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as='div'>
             Profile
@@ -53,15 +64,26 @@ const DashSidebar = () => {
             </Link>
           )}
          {currentUser.isAdmin && (
-         <Link to='/dashboard?tab=users'>
-              <Sidebar.Item
-                active={tab === 'users'}
-                icon={HiUserGroup}
-                as='div'
-              >
-                Users
-              </Sidebar.Item>
-            </Link>
+              <>
+              <Link to='/dashboard?tab=users'>
+                <Sidebar.Item
+                  active={tab === 'users'}
+                  icon={HiUserGroup}
+                  as='div'
+                >
+                  Users
+                </Sidebar.Item>
+              </Link>
+              <Link to='/dashboard?tab=comments'>
+                <Sidebar.Item
+                  active={tab === 'comments'}
+                  icon={HiAnnotation}
+                  as='div'
+                >
+                  Comments
+                </Sidebar.Item>
+              </Link>
+            </>
           )}
          <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleLogout} >
             Logout
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,8 @@ import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
 import DashUsers from "../components/DashUsers";
+import DashComments from "../components/DashComments";
+import DashboardComp from "../components/DashboardComp";
 
 const Dashboard = () => {
   const location = useLocation();
@@ -25,6 +27,10 @@ const Dashboard = () => {
       {tab === 'posts' && <DashPosts />}
       {/* users... */}
       {tab === 'users' && <DashUsers />}
+       {/* comments  */}
+       {tab === 'comments' && <DashComments />}
+            {/* dashboard component */}
+      {tab === 'dash' && <DashboardComp />}
     </div>
   )
 }
