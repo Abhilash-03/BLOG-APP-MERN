d41d5a73ed388e79dcd714aa96b577476ac92e10
Abhilash <abhilashjha335@gmail.com>
2024-04-06 18:27:05 +0530

    complete search functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
 import PostPage from './pages/PostPage'
 import ScrollToTop from './components/ScrollToTop'
+import Search from './pages/Search'
 
 function App() {
 
@@ -25,6 +26,7 @@ function App() {
        <Route path='/about' element={<About />} />
        <Route path='/login' element={<Login />} />
        <Route path='/sign-up' element={<Singup />} />
+       <Route path='/search' element={<Search />} />
        <Route element={<PrivateRoute />}>
          <Route path='/dashboard' element={<Dashboard />} />
        </Route>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,29 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react"
-import { Link, useLocation } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import {AiOutlineSearch} from 'react-icons/ai'
 import {FaMoon, FaSun} from 'react-icons/fa'
 import { useSelector, useDispatch } from 'react-redux'
 import { toggleTheme } from "../redux/theme/themeSlice"
 import { logoutSuccess } from "../redux/user/userSlice"
+import { useEffect, useState } from "react"
 
 const Header = () => {
   const { currentUser } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
   const { theme } = useSelector(state => state.theme);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   const handleLogout = async() => {
     try {
      const res = await fetch(`/api/v1/user/logout`, {
@@ -27,17 +40,27 @@ const Header = () => {
       console.log(error.message);
     }
 }
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const urlParams = new URLSearchParams(location.search);
+  urlParams.set('searchTerm', searchTerm);
+  const searchQuery = urlParams.toString();
+  navigate(`/search?${searchQuery}`);
+};
+
   return (
     <Navbar className='border-b-2'>
       <Link to='/' className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white font-mono" >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white font-serif">Abhilash's</span>Blog
       </Link>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextInput
          type="text"
          placeholder="Search..."
          rightIcon={AiOutlineSearch}
          className="hidden lg:inline"
+         value={searchTerm}
+         onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
